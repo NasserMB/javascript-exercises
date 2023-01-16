88bc1101ab2c5fbb1821b820d80363abcc39407a
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-16 22:53:38 +0300

    Start working on 02 JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(txt, x) {
+    let resultString = '';
 
+    for (let i=0; i < x; i++) {
+        if (x<0) {
+            resultString = 'ERROR';
+            break;
+        }
+        resultString += txt;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
