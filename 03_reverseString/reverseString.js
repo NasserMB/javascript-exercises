d0f47b3be06e04ae951e810d267ddba9a5ac772f
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-17 09:08:50 +0300

    Complete exercise 03 of JS foundation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const textIndex = text.length - 1;
+    let reverseText = ''
 
+    for (i = textIndex; i >= 0; i--) {
+        reverseText += text[i];
+    };
+
+    return reverseText;
 };
 
 // Do not edit below this line
