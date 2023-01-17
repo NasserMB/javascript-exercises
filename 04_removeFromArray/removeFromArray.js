da7f13cbcbcfebbe0e2c81cfd63b9ef9f3be1d48
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-17 11:36:02 +0300

    Complete exercise 04 from JS funamentals

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(startList, ...excludeList) {
+    let excludeIndex = -1;
+    
+    for (i = 0; i < excludeList.length; i++) {
+        excludeIndex = startList.indexOf(excludeList[i]);
+        
+        if (excludeIndex !== -1) {
+            startList.splice(excludeIndex,1);
+        };
+    };
 
+    return startList;
 };
 
 // Do not edit below this line
