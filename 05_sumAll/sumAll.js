3ffb01d04476f36529c6fc9dc19604589ed1d97f
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-17 12:43:10 +0300

    Complete exercise 05 of JS fundamentals

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sumOfAll = 0;
+    let x;
+    let y;
 
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    };
+
+    if ((compareNums(a ,b)) == -1) {
+        x = b;
+        y = a;
+    } else {
+        x = a;
+        y = b;
+    };
+    
+
+    for (i = x; i <= y; i++) {
+        sumOfAll += i;
+    };
+
+    return sumOfAll;
+};
+   
+function compareNums (numOne, numTwo) {
+
+    if (numOne > numTwo) {
+        return -1;
+    };
+
+    return 1;
 };
 
 // Do not edit below this line
