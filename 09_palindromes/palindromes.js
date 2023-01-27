6940492d16aaca1ae07fcd8a33938bfa6ca22ed6
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-27 22:56:44 +0300

    Complete exercise 09 of javascript-exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let remNonLetters = str.replace(/[^a-zA-Z0-9]/g, '');
+    let lowerCase = remNonLetters.toLowerCase();
+    
+    let splitStr = lowerCase.split("");
+    let reverseStr = splitStr.reverse().join("");
+    return lowerCase === reverseStr;
 };
 
 // Do not edit below this line
