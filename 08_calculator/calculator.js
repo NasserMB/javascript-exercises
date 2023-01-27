f21d94aca7ebaf178fe6c54f81f28e8c3938c97f
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-27 22:13:16 +0300

    Complete exercise 08 of javascript-exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  const sumOfNumbers = numbers.reduce((total, number) => total + number, 0);
+  return sumOfNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const productOfNumbers = numbers.reduce((total, number) => total * number, 1);
+  return productOfNumbers;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1;
+  for (let f = number; f > 0; f--) {
+    total *= f;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
