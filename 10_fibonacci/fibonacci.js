07e05d121a5b7c7e9e9d1a55ac29188130463959
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-28 09:11:22 +0300

    Complete exercise 10 of javascript-exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
+    let result = 1;
+    const pos = parseInt(num, 10);
 
+    if (pos <= 0) return "OOPS";
+
+    for (let i = 1; i < pos; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
