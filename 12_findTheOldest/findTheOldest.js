c73a73cd033b8172c04c06d88783df347390e225
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-29 20:43:15 +0300

    Complete exercise 12 of javascript-exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldestPerson = countYears(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentPerson = countYears(person.yearOfDeath, person.yearOfBirth);
+        return oldestPerson < currentPerson ? person : oldest;
+    })
 };
 
+const countYears = function(death, birth){
+    if (death === undefined) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
